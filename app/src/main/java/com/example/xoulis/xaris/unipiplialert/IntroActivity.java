4f0f358fd3214e664409e4bec71290b88b36d52f
Charis Ch <charis1331@users.noreply.github.com>
2017-12-29 17:09:10 +0200

    added xxhdpi for dimens.xml, changed layout of Slide1, fixed setError padding,

--- app/src/main/java/com/example/xoulis/xaris/unipiplialert/IntroActivity.java
+++ app/src/main/java/com/example/xoulis/xaris/unipiplialert/IntroActivity.java
@@ -7,6 +7,7 @@ import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -30,6 +31,8 @@ public class IntroActivity extends AppIntro implements TextWatcher {
     private boolean contact1IsValid = false;
     private boolean contact2IsValid = false;
 
+    private Boolean addDefaultTextToContacts = true;
+
     private static final String USERNAME_FIELD = "username";
     private static final String PASSWORD_FIELD = "password";
 
@@ -42,6 +45,7 @@ public class IntroActivity extends AppIntro implements TextWatcher {
         addSlide(SampleSlide.newInstance(R.layout.slide3));
 
         showSkipButton(false);
+        setProgressButtonEnabled(false);
         setSeparatorColor(Color.TRANSPARENT);
     }
 
@@ -61,16 +65,14 @@ public class IntroActivity extends AppIntro implements TextWatcher {
             // Set listeners to all the views
             addTextChangeListeners();
 
-            // Set default text for contacts editTexts
-            setDefaultTextForContactsFields();
+            // Add default text to Contact fields, ONLY the FIRST time this class is called
+            if (addDefaultTextToContacts) {
+                // Set default text for contacts editTexts
+                setDefaultTextForContactsFields();
+                addDefaultTextToContacts = false;
+            }
         }
 
-    }
-
-    @Override
-    public void onDonePressed(Fragment currentFragment) {
-        super.onDonePressed(currentFragment);
-        finish();
     }
 
     private void initViews() {
@@ -93,8 +95,6 @@ public class IntroActivity extends AppIntro implements TextWatcher {
         contact2EditText.addTextChangedListener(this);
     }
 
-
-    // TODO Check for 1-digit, 3-digits country codes!!
     private void setDefaultTextForContactsFields() {
         String defaultText = "+" + GetCountryCodeNumber.getCallCode(this);
         contact1EditText.setText(defaultText);
@@ -131,21 +131,21 @@ public class IntroActivity extends AppIntro implements TextWatcher {
             inputOfLengthFour(USERNAME_FIELD);
 
         } else if (editable == passwordEditText.getEditableText()) {
-            // Check if input is at least of length 4
+            // Check if input is at least of length 2
             inputOfLengthFour(PASSWORD_FIELD);
 
         } else if (editable == contact1EditText.getEditableText()) {
             // First check for existence of + symbol
             symbolExistence(1, editable);
 
-            // Then check for input validity [must be 13 chars ("+" + country code +  number)]
-            inputHasThirteenDigits(1);
+            // Then check for input validity [must be at least 2 char (including country code)]
+            inputOfLengthTwo(1);
         } else if (editable == contact2EditText.getEditableText()) {
             // First check for existence of + symbol
             symbolExistence(2, editable);
 
-            // Then check for input validity (must have 13 chars)
-            inputHasThirteenDigits(2);
+            // Then check for input validity [must be at least 2 char (including country code)]
+            inputOfLengthTwo(2);
         }
     }
 
@@ -155,7 +155,7 @@ public class IntroActivity extends AppIntro implements TextWatcher {
                 usernameTextInputLayout.setError(getString(R.string.username_text_input_layout_error_message));
                 usernameIsValid = false;
             } else {
-                usernameTextInputLayout.setError(null);
+                usernameTextInputLayout.setErrorEnabled(false);
                 usernameIsValid = true;
             }
         } else {
@@ -163,7 +163,7 @@ public class IntroActivity extends AppIntro implements TextWatcher {
                 passwordTextInputLayout.setError(getString(R.string.password_text_input_layout_error_message));
                 passIsValid = false;
             } else {
-                passwordTextInputLayout.setError(null);
+                passwordTextInputLayout.setErrorEnabled(false);
                 passIsValid = true;
             }
         }
@@ -188,26 +188,21 @@ public class IntroActivity extends AppIntro implements TextWatcher {
         }
     }
 
-    // TODO Check for 1-digit, 3-digits country codes!!
-    private void inputHasThirteenDigits(int contactNo) {
+    private void inputOfLengthTwo(int contactNo) {
         if (contactNo == 1) {
-            if (contact1EditText.getText().length() < 3) {
+            if (contact1EditText.getText().length() < 2) {
                 contact1TextInputLayout.setError(getString(R.string.contact_without_country_code_error_message));
                 contact1IsValid = false;
-            } else if (contact1EditText.getText().length() < 13) {
-                contact1TextInputLayout.setError(getString(R.string.contact_with_country_code_error_message));
             } else {
-                contact1TextInputLayout.setError(null);
+                contact1TextInputLayout.setErrorEnabled(false);
                 contact1IsValid = true;
             }
         } else {
-            if (contact2EditText.getText().length() < 3) {
+            if (contact2EditText.getText().length() < 2) {
                 contact2TextInputLayout.setError(getString(R.string.contact_without_country_code_error_message));
                 contact2IsValid = false;
-            } else if (contact2EditText.getText().length() < 13) {
-                contact2TextInputLayout.setError(getString(R.string.contact_with_country_code_error_message));
             } else {
-                contact2TextInputLayout.setError(null);
+                contact2TextInputLayout.setErrorEnabled(false);
                 contact2IsValid = true;
             }
         }
